340045255ca8e6cbe8ad4ab9a7e9883d067035e6
zwd-admin <810095178@qq.com>
2018-04-12 10:42:31 +0800

    zwd

---
+++ src/main/java/com/zwd/App.java
@@ -0,0 +1,12 @@
+package com.zwd;
+
+/**
+ * @author zwd
+ * @date 2018/4/12 10:34
+ * @Email lovejavazwd@gmail.com
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello jenkins");
+    }
+}
